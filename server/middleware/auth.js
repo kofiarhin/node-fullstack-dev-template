d11e6ac65725841c9351ad3f687f512ee0f26d7d
unknown <mysocialmedia69@yahoo.com>
2021-02-12 09:43:08 +0000

    update

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,40 +1,33 @@
-const jwt = require("jsonwebtoken")
-const User = require("../model/user")
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
 
+const auth = async (req, res, next) => {
+  try {
+    let token;
 
+    if (process.env.environment === "test") {
+      token = req.header("token");
+    } else if (
+      process.env.environment === "dev" ||
+      process.env.environment === "prod"
+    ) {
+      token = req.cookies.token;
+    }
 
+    const payload = jwt.verify(token, process.env.jwt_secret);
 
-const auth = async (req, res, next) =>  {
+    const { _id } = payload;
 
-   try { 
+    const user = await User.findById(_id);
 
-             let token;
-
-            if(process.env.environment === "test") {
-               token = req.header("token");
-            }
-
-
-            else if(process.env.environment === "dev") {
-               token  = req.cookies.token;
-            }
-
-             const payload = jwt.verify(token, process.env.jwt_secret);
-
-             const { _id }  = payload;
-
-             const user = await User.findById(_id);
-
-             req.user = user;
-             req.token = token;
-   res.locals.user = user;
-             next()
-   }catch( error) {
-      res.locals.user = ""
-      res.status(404).send({  error: "invalid credentials"})
-   }
-
-
-}
+    req.user = user;
+    req.token = token;
+    res.locals.user = user;
+    next();
+  } catch (error) {
+    res.locals.user = "";
+    res.status(404).send({ error: "invalid credentials" });
+  }
+};
 
 module.exports = auth;
